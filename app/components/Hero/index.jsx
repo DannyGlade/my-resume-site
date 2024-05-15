ed88072bca64d46520773b4afb35ef529d7fe23a
DannyGlade <darshannariya12@gmail.com>
2024-05-15 17:44:44 -0400

    apos added

--- app/components/Hero/index.jsx
+++ app/components/Hero/index.jsx
@@ -14,7 +14,7 @@ export default function Hero() {
                 <Image src="/DarshanNariyaMobile.png" alt="Hero" fill objectFit='contain' className={styles.hero__image} />
             </div>
             <div className={styles.hero__content}>
-                <div className={styles.name__carousal}> Hey, I'm &nbsp;
+                <div className={styles.name__carousal}> Hey, I&apos;m &nbsp;
                     <Splide options={{ type: 'loop', direction: 'ttb', autoHeight: true, heightRatio: 1, pagination: false, arrows: false, autoplay: true, interval: 3000, gap: '0em', mediaQuery:'min', breakpoints: {320:{start: 0}, 1440: { start: 3 } } }}>
                         <SplideSlide tabIndex={0}>
                             <h1>Darshan</h1>

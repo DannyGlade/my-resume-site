04a3b1ccb40dfb00e96070fc36878951311ef4f8
DannyGlade <darshannariya12@gmail.com>
2024-05-15 18:25:10 -0400

    refactor: Update Hero component for responsive carousal design and image display

--- app/components/Hero/index.jsx
+++ app/components/Hero/index.jsx
@@ -15,7 +15,7 @@ export default function Hero() {
             </div>
             <div className={styles.hero__content}>
                 <div className={styles.name__carousal}> Hey, I&apos;m &nbsp;
-                    <Splide options={{ type: 'loop', direction: 'ttb', autoHeight: true, heightRatio: 1, pagination: false, arrows: false, autoplay: true, interval: 3000, gap: '0em', mediaQuery:'min', breakpoints: {320:{start: 0}, 1440: { start: 3 } } }}>
+                    <Splide options={{ type: 'loop', direction: 'ttb', autoHeight: true, heightRatio: 1, pagination: false, arrows: false, autoplay: true, interval: 3000, gap: '0em', mediaQuery: 'min', breakpoints: { 320: { start: 0 }, 768: { start: 3 }, 1440: { start: 3 } } }}>
                         <SplideSlide tabIndex={0}>
                             <h1>Darshan</h1>
                         </SplideSlide>

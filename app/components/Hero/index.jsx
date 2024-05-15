40c0a23543192765b018fc5790106a9d9f41f11f
DannyGlade <darshannariya12@gmail.com>
2024-05-15 17:18:04 -0400

    always display name first in hero carousal, and tab font fix

--- app/components/Hero/index.jsx
+++ app/components/Hero/index.jsx
@@ -15,8 +15,8 @@ export default function Hero() {
             </div>
             <div className={styles.hero__content}>
                 <div className={styles.name__carousal}> Hey, I'm &nbsp;
-                    <Splide options={{ type: 'loop', direction: 'ttb', autoHeight: true, heightRatio:1 ,pagination: false, arrows: false, autoplay: true, interval: 3000, gap:'0em', start:0 }}>
-                        <SplideSlide>
+                    <Splide options={{ type: 'loop', direction: 'ttb', autoHeight: true, heightRatio: 1, pagination: false, arrows: false, autoplay: true, interval: 3000, gap: '0em', mediaQuery:'min', breakpoints: {320:{start: 0}, 1440: { start: 3 } } }}>
+                        <SplideSlide tabIndex={0}>
                             <h1>Darshan</h1>
                         </SplideSlide>
                         <SplideSlide>

aeafeb6e4f5211824029c377dc3de3b45d342b6a
DannyGlade <darshannariya12@gmail.com>
2024-09-04 15:31:03 -0400

    added form in contact section, and link name in cards

--- app/components/Contact/ContactCard.jsx
+++ app/components/Contact/ContactCard.jsx
@@ -6,8 +6,9 @@ import { SocialIcon } from 'react-social-icons'
 
 export default function ContactCard({ name, link }) {
     return (
-        <Link className={styles.contact_card} href={link} target='_blank'>
-                <SocialIcon style={{ height: 50, width: 50 }} url={link} />
+        <Link className={styles.contact_card} href={link} onClick={() => console.log("hi")} target='_blank'>
+            <SocialIcon style={{ height: 50, width: 50 }} url={link} />
+            <p>{name}</p>
         </Link>
     )
 }
--- app/components/Contact/index.jsx
+++ app/components/Contact/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import styles from './styles.module.css'
 import ContactCard from './ContactCard'
+import { TextInput, Textarea, SimpleGrid, Group, Button } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { MdMail } from "react-icons/md";
 
 export default function Contact() {
+    const form = useForm({
+        initialValues: {
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        },
+        validate: {
+            name: (value) => value.trim().length < 2,
+            email: (value) => !/^\S+@\S+$/.test(value),
+            subject: (value) => value.trim().length === 0,
+        },
+    });
 
     const contacts = [
         {
@@ -19,10 +35,77 @@ export default function Contact() {
         }
     ]
 
+    const handleSubmit = (event) => {
+        // event.preventDefault();
+
+        console.log(form.isValid());
+
+        if (form.isValid()) {
+            console.log(form.values);
+
+            let mailtoLink = contacts[0].link + '?subject=' + encodeURIComponent(form.values.subject) + '&body=' + encodeURIComponent(form.values.message);
+
+            window.open(mailtoLink, '_blank');
+            form.reset();
+        }
+
+
+    }
+
     return (
         <div id='contact' className={styles.contact}>
             <div className={styles.contact__heading}>
                 <h2>say hi!...</h2>
+            </div>
+            <div className={{}}>
+                <form onSubmit={form.onSubmit(handleSubmit)} className={[styles.form__container]}>
+                    <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl" >
+                        <TextInput
+                            label="Name"
+                            placeholder="Your name"
+                            name="name"
+                            variant="filled"
+                            {...form.getInputProps('name')}
+                        />
+                        <TextInput
+                            label="Email"
+                            placeholder="Your email"
+                            name="email"
+                            variant="filled"
+                            {...form.getInputProps('email')}
+                        />
+
+                    </SimpleGrid>
+                    <TextInput
+                        label="Subject"
+                        placeholder="Subject"
+                        mt="md"
+                        name="subject"
+                        variant="filled"
+                        {...form.getInputProps('subject')}
+                    />
+                    <Textarea
+                        mt="md"
+                        label="Message"
+                        placeholder="Your message"
+                        maxRows={10}
+                        minRows={5}
+                        autosize
+                        name="message"
+                        variant="filled"
+                        {...form.getInputProps('message')}
+                    />
+
+                    <Group justify="center" mt="xl">
+                        <Button type="submit" variant="default" size="md">
+                            <MdMail style={{ scale: 1.2, marginRight: '10px' }} />
+                            Send Message
+                        </Button>
+                    </Group>
+                </form>
+            </div>
+            <div className={styles.contact__heading}>
+                <h2>or find me here...</h2>
             </div>
             <div className={styles.contact__container}>
                 {contacts.map((contact, index) => (

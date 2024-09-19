6d7d572f6df92c74a97e73e145726ecc87894ec5
DannyGlade <darshannariya12@gmail.com>
2024-09-19 18:38:57 -0400

    Refactor SkillCard component to use Mantine Card component

--- app/components/Skills/SkillCard.jsx
+++ app/components/Skills/SkillCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import styles from './styles.module.css'
 import 'devicon/devicon.min.css'
+import { Card } from '@mantine/core'
 
 export default function SkillCard({ skill, type, colored, category }) {
     return (
-        <div className={[styles.skill_card, category].join(' ')}>
+        <Card className={[styles.skill_card, category].join(' ')}>
             <div className={styles.skill_card__icon}>
                 <i className={`devicon-${skill.toLowerCase()}-${type} ${colored ? 'colored' : ''}`}></i>
             </div>
             <div className={styles.skill_card__name}>
                 {skill}
             </div>
-        </div>
+        </Card>
     )
 }
--- app/components/Skills/index.tsx
+++ app/components/Skills/index.tsx
@@ -1,16 +1,20 @@
 "use client";
-import React, { useRef, useState, useEffect, useMemo, use } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import SkillCard from "./SkillCard";
 import { Button, useComputedColorScheme } from "@mantine/core";
 import { MdFilterAlt } from "react-icons/md";
 import type Isotope from "isotope-layout";
-import skills, { FilterKey } from "../../../data/Skills";
+import skills, { FilterKey } from "./../../../data/Skills";
 
 export default function Skills() {
    const computedColorScheme = useComputedColorScheme("dark", {
       getInitialValueInEffect: true,
    });
+
+   const [filters, setFilters] = useState<string[]>(
+      Object.keys(FilterKey).filter((key) => isNaN(Number(key)))
+   );
 
    const isotope = useRef<Isotope | null>(null);
 
@@ -29,7 +33,7 @@ export default function Skills() {
       return () => {
          isotope.current?.destroy();
       };
-   }, []);
+   }, [computedColorScheme]);
 
    const setIsotope = (Isotope) => {
       try {
@@ -42,15 +46,14 @@ export default function Skills() {
                gutter: 8,
             },
          });
-         //  isotope.current.arrange({ filter: "*" });
       } catch (error) {
-         //  if (error.message === "No layout mode: packery") {
-         //     isotope.current = new Isotope(`.${styles.skills__container}`, {
-         //        itemSelector: `.${styles.skill_card}`,
-         //        percentagePosition: true,
-         //     });
-         //     console.error(error);
-         //  }
+         if (error.message === "No layout mode: packery") {
+            isotope.current = new Isotope(`.${styles.skills__container}`, {
+               itemSelector: `.${styles.skill_card}`,
+               percentagePosition: true,
+            });
+            console.error(error);
+         }
       }
    };
 
@@ -60,7 +63,7 @@ export default function Skills() {
       } else {
          isotope.current?.arrange({ filter: `.${FilterKey[filterKey]}` });
       }
-   }, [filterKey, isotope.current]);
+   }, [filterKey]);
 
    return (
       <div id="skills" className={styles.skills}>
@@ -75,24 +78,13 @@ export default function Skills() {
             >
                <MdFilterAlt style={{ minWidth: "2em", minHeight: "2em" }} />
             </Button>
-            {Object.keys(FilterKey)
-               .filter((key) => isNaN(Number(key)))
-               .map((key) => (
-                  <Button
-                     key={key}
-                     variant={
-                        filterKey === FilterKey[key]
-                           ? computedColorScheme === "dark"
-                              ? "white"
-                              : "dark"
-                           : "default"
-                     }
-                     onClick={() => setFilterKey(FilterKey[key])}
-                  >
-                     {key === "*" ? "All" : key}
-                  </Button>
-               ))}
-            {/* </ButtonGroup> */}
+            {filters && (
+               <Filters
+                  filters={filters}
+                  filterKey={filterKey}
+                  setFilterKey={setFilterKey}
+               />
+            )}
          </div>
 
          <div className={styles.skills__container}>
@@ -110,30 +102,34 @@ export default function Skills() {
                />
             ))}
          </div>
-
-         {/* <Isotope options={{ itemSelector: '.grid-item', layoutMode: 'masonary' }}>
-                <div className={styles.skills__grid}>
-                    {skills.map((skill, index) => (
-                        <SkillCard key={index} skill={skill} />
-                    ))}
-                </div>
-            </Isotope> */}
-
-         {/* <div className={styles.skills__container}>
-                <div className={styles.skills__filter}>
-                    <button data-filter="*" className={styles.active} onClick={() => setFilterKey('*')}>All</button>
-
-                    <button data-filter=".frontend" onClick={() => setFilterKey('frontend')}
-                    >Frontend</button>
-                    <button data-filter=".backend">Backend</button>
-                    <button data-filter=".design">Design</button>
-                </div>
-                <div className={styles.skills__grid} ref={ele}>
-                    {skills.map((skill, index) => (
-                        <SkillCard key={index} skill={skill} />
-                    ))}
-                </div>
-            </div> */}
       </div>
    );
 }
+
+type FiltersProps = {
+   filters: string[];
+   filterKey: FilterKey;
+   setFilterKey: React.Dispatch<React.SetStateAction<FilterKey>>;
+};
+
+const Filters = ({ filters, filterKey, setFilterKey }: FiltersProps) => {
+   const computedColorScheme = useComputedColorScheme("dark", {
+      getInitialValueInEffect: true,
+   });
+
+   return filters.map((key) => (
+      <Button
+         key={key}
+         variant={
+            filterKey === FilterKey[key]
+               ? computedColorScheme === "dark"
+                  ? "white"
+                  : "dark"
+               : "default"
+         }
+         onClick={() => setFilterKey(FilterKey[key])}
+      >
+         {key === "*" ? "All" : key}
+      </Button>
+   ));
+};

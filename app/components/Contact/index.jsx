5ddb226caa67eed4d6ed05622b7ed5aa6e1c800a
DannyGlade <darshannariya12@gmail.com>
2024-09-19 17:33:43 -0400

    Refactor contact form layout and structure

--- app/components/Contact/index.jsx
+++ app/components/Contact/index.jsx
@@ -57,53 +57,51 @@ export default function Contact() {
             <div className={styles.contact__heading}>
                 <h2>say hi!...</h2>
             </div>
-            <div className={{}}>
-                <form onSubmit={form.onSubmit(handleSubmit)} className={[styles.form__container]}>
-                    <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl" >
-                        <TextInput
-                            label="Name"
-                            placeholder="Your name"
-                            name="name"
-                            variant="filled"
-                            {...form.getInputProps('name')}
-                        />
-                        <TextInput
-                            label="Email"
-                            placeholder="Your email"
-                            name="email"
-                            variant="filled"
-                            {...form.getInputProps('email')}
-                        />
-
-                    </SimpleGrid>
+            <form onSubmit={form.onSubmit(handleSubmit)} className={[styles.form__container]}>
+                <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl" >
                     <TextInput
-                        label="Subject"
-                        placeholder="Subject"
-                        mt="md"
-                        name="subject"
+                        label="Name"
+                        placeholder="Your name"
+                        name="name"
                         variant="filled"
-                        {...form.getInputProps('subject')}
+                        {...form.getInputProps('name')}
                     />
-                    <Textarea
-                        mt="md"
-                        label="Message"
-                        placeholder="Your message"
-                        maxRows={10}
-                        minRows={5}
-                        autosize
-                        name="message"
+                    <TextInput
+                        label="Email"
+                        placeholder="Your email"
+                        name="email"
                         variant="filled"
-                        {...form.getInputProps('message')}
+                        {...form.getInputProps('email')}
                     />
 
-                    <Group justify="center" mt="xl">
-                        <Button type="submit" variant="default" size="md">
-                            <MdMail style={{ scale: 1.2, marginRight: '10px' }} />
-                            Send Message
-                        </Button>
-                    </Group>
-                </form>
-            </div>
+                </SimpleGrid>
+                <TextInput
+                    label="Subject"
+                    placeholder="Subject"
+                    mt="md"
+                    name="subject"
+                    variant="filled"
+                    {...form.getInputProps('subject')}
+                />
+                <Textarea
+                    mt="md"
+                    label="Message"
+                    placeholder="Your message"
+                    maxRows={10}
+                    minRows={5}
+                    autosize
+                    name="message"
+                    variant="filled"
+                    {...form.getInputProps('message')}
+                />
+
+                <Group justify="center" mt="xl">
+                    <Button type="submit" variant="default" size="md">
+                        <MdMail style={{ scale: 1.2, marginRight: '10px' }} />
+                        Send Message
+                    </Button>
+                </Group>
+            </form>
             <div className={styles.contact__heading}>
                 <h2>or find me here...</h2>
             </div>
